6c887581b47d4122a078cecb7a4f6c5507837a76
Carolyn <carolynwarburton@hotmail.com>
2016-12-06 07:38:02 +0000

    More tweaks

--- sage/js/custom.js
+++ sage/js/custom.js
@@ -1,4 +1,18 @@
 $(function () {
-    // Handler for .ready() called.
-    alert("ready");
+    // accounting-filter-button
+    $("#btn-filter-1").on("click", function () {
+        $("#pnl-card-3").hide();
+        $("#pnl-card-1").show();
+        $("#pnl-card-2").show();
+    });
+    $("#btn-filter-2").on("click", function () {
+        $("#pnl-card-1").hide();
+        $("#pnl-card-2").show();
+        $("#pnl-card-3").show();
+    });
+    $("#btn-filter-3").on("click", function () {
+        $("#pnl-card-2").hide();
+        $("#pnl-card-1").show();
+        $("#pnl-card-3").show();
+    });
 });
