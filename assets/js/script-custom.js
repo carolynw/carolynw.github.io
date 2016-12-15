6ef33035212ec1e7e60ed3eb9381a82cd880be9d
Carolyn <carolynwarburton@hotmail.com>
2016-12-15 19:25:52 +0000

    Added show button

--- assets/js/script-custom.js
+++ assets/js/script-custom.js
@@ -1,4 +1,14 @@
-(function($) {
+(function ($) {
+
+    $(".btn-show").on("click", function () {
+        $(this).hide();
+        $(this).parent().find(".carousel-caption").show();
+    });
+
+    $(".btn-hide").on("click", function () {
+        $(this).parent().parent().find(".btn-show").show();
+        $(this).parent().parent().find(".carousel-caption").hide();
+    });
 $("ul.filter-author>li>a").on("click",function(){
 	var clicked = $(this).text();
 	$("ul.filter-author>li").removeClass("active");
